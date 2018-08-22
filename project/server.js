08e5eec43db846bdf0cc6bb072d1c8e4af43777b
adhondt42 <adhondt@student.42.fr>
2018-08-22 19:02:37 +0200

    Middleware error OK

--- project/server.js
+++ project/server.js
@@ -17,23 +17,19 @@ app.use(session({
 	saveUninitialized: true,
 	cookie: { secure: false }
   }))
+app.use(require('./middlewares/flash'))
 
 // Route
 app.get('/', (req, res) => {
     console.log("Someone on main page")
-    if (req.session.error)
-    {
-        res.locals.error = req.session.error
-        req.session.error = undefined
-    }
 	res.render('pages/index')
 })
 
 app.post('/' , (req, res) => {
 	if (req.body.message === undefined || req.body.message === '') {
-        req.session.error = "il y a une big mistake"
-		res.redirect('/')
-	}
+        req.flash('error', 'Mais de quelle Dhondterie parle-t-on ?')
+        res.redirect('/')
+    }
 	else
 		res.redirect("http://google.com")
 	console.log("Someone on '/'")
