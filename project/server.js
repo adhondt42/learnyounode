d7f2847059d0b29efb142eb3df9a324871a22c4e
Axel DHONDT <adhondt@e3r4p22.42.fr>
2018-08-22 16:31:23 +0200

    auto commit

--- project/server.js
+++ project/server.js
@@ -1,30 +1,37 @@
 let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
+let session = require('express-session')
 
 
-// Template engine 
+// Template engine
 app.set('view engine', 'ejs')
+
 
 // Middleware
 app.use('/assets', express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
-
+app.use(session({
+	secret: 'beer',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: false }
+  }))
 
 // Route
 app.get('/', (req, res) => {
-    res.render('pages/index')
+	res.render('pages/index')
 })
 
 app.post('/' , (req, res) => {
-    if (req.body.message === undefined || req.body.message === '') {
-        res.redirect('/')
-    }
-    else
-        res.redirect("http://google.com")
-    console.log("Someone on '/'")
+	if (req.body.message === undefined || req.body.message === '') {
+		res.redirect('/')
+	}
+	else
+		res.redirect("http://google.com")
+	console.log("Someone on '/'")
 
 })
 
 
-app.listen(8000)  
+app.listen(8000)
