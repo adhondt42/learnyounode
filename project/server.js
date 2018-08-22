87dd33328fe7ba00f3cdd089c4eaac60d1fb7d31
adhondt42 <adhondt@student.42.fr>
2018-08-22 16:55:31 +0200

    start cooking

--- project/server.js
+++ project/server.js
@@ -20,11 +20,18 @@ app.use(session({
 
 // Route
 app.get('/', (req, res) => {
+    console.log("Someone on main page")
+    if (req.session.error)
+    {
+        res.locals.error = req.session.error
+        req.session.error = undefined
+    }
 	res.render('pages/index')
 })
 
 app.post('/' , (req, res) => {
 	if (req.body.message === undefined || req.body.message === '') {
+        req.session.error = "il y a une big mistake"
 		res.redirect('/')
 	}
 	else
