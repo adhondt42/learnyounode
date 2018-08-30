08632c6119d7ac1869ab4db11cc73cb5c141a1f7
adhondt42 <adhondt@student.42.fr>
2018-08-30 17:10:44 +0200

    Chat socket.io OK

--- projet_chat/server.js
+++ projet_chat/server.js
@@ -1,31 +1,47 @@
-var http = require('http');
+var express = require('express')
+var app = express()
+var server = require('http').Server(app);
 var fs = require('fs');
-var ent = require('ent') // evite injection de js ??
+var ent = require('ent')                                                        // evite injection de js ??
 
-var server = http.createServer(function(req, res) {
-    fs.readFile('./views/index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-});
+// Setting express/template
+app.set('view engine', 'ejs')
+app.use(express.static('public'))                                               // dossier contient les statics
 
+app.get('/', (req, res) => {
+    // res.sendFile('./views/index.ejs' , { root : __dirname});                 // __dirname = current directory
+    res.render('index') // retourne le template
+})
 
-var io = require('socket.io').listen(server) // Chargement de socket.io
+// Setting socket
+var io = require('socket.io').listen(server)                                    // Chargement de socket.io
+var user_list = []
 
-io.sockets.on('connection', function (socket) {
+// On new socket
+io.sockets.on('connection', function (socket) {                                 // une socket par user 
 
+    for (var k = 0; k < user_list.length; k++) {
+        socket.emit('new_client', {pseudo: user_list[k], txt_area: 0})          
+    }
     socket.on('new_client', (pseudo) => {
         pseudo = ent.encode(pseudo)
         socket.pseudo = pseudo
-        socket.broadcast.emit('new_client', pseudo)
+        io.sockets.emit('new_client', {pseudo: pseudo, txt_area: 1})            // Tous les user dont moi
+        user_list.push(pseudo)
     })
 
     socket.on('message', (message) => {
         message = ent.encode(message)
-        console.log(message)
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message})
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message}) // tous les usr sauf moi
+    })
+
+    socket.on('disconnect', () => {
+        var index = user_list.indexOf(socket.pseudo);
+        if (index > -1) {
+          user_list.splice(index, 1);
+        }
+        io.sockets.emit('usr_disconnect', socket.pseudo)
     })
 })
-
 
 server.listen(8888)
